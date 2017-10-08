9f11fd86b535cf75df3298e08259d2eaa1083e57
Philip Trauner <philip.trauner@aol.com>
2017-10-08 22:44:35 +0200

    Upped version to 0.2.1

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ preserving* network protocol framework built on top of *WebSockets*. It facilita
 
 setup(
 	name="highway.py",
-	version="0.2.0",
+	version="0.2.1",
 	author="Philip Trauner",
 	author_email="philip.trauner@arztpraxis.io",
 	url="https://github.com/PhilipTrauner/highway.py",
