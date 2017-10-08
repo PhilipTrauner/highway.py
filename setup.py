2af4bc90e3a112613d9485fc85fb63d8ecc3531e
Philip Trauner <philip.trauner@aol.com>
2017-10-08 19:51:34 +0200

    Upped version to 0.2.0, added long description, added new dependencies

--- setup.py
+++ setup.py
@@ -4,22 +4,37 @@ from sys import version_info
 
 
 description = "Asynchronous, route based, and data type preserving network protocol."
+long_description = """
+============
+highway.py
+============
 
+.. image:: https://cloud.githubusercontent.com/assets/9287847/26756438/93c1ff54-48a2-11e7-981e-37aeb7b2383c.png
+   :height: 150px
+   :alt: highway banner
+   :align: center
+
+
+**highway** is a *lightweight*, *route-based* and *data-type
+preserving* network protocol framework built on top of *WebSockets*. It facilitates routes as a means of data transmission.
+"""
 
 setup(
 	name="highway.py",
-	version="0.1",
+	version="0.2.0",
 	author="Philip Trauner",
-	author_email="philip.trauner@aol.com",
+	author_email="philip.trauner@arztpraxis.io",
 	url="https://github.com/PhilipTrauner/highway.py",
 	packages=find_packages(),
 	description=description,
-	install_requires=["ws4py"],
+	long_description=long_description,
+	install_requires=[
+		"websockets==3.4",
+		"ujson==1.35"
+	],
 	license="MIT",
 	classifiers=[
-		"Development Status :: 3 - Alpha",
-		"Programming Language :: Python :: 3.4",
-		"Programming Language :: Python :: 3.5",
+		"Development Status :: 4 - Beta",
 		"Programming Language :: Python :: 3.6",
 		"Intended Audience :: Developers",
 		"License :: OSI Approved :: MIT License",
